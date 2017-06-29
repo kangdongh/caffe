6939825108c3d66af8ffaefba872b8d20a34694b
Donghyun, Kang <kangdongh@snu.ac.kr>
2017-06-29 17:31:58 +0900

    script error fixed

--- scripts/KDH/t2caffe_inception.py
+++ scripts/KDH/t2caffe_inception.py
@@ -5,7 +5,31 @@ import numpy as np
 import os
 import lutorpy as lua
 import m2list
-import modules2net
+
+def modules2params(net, convparams, scaleparams, convmodules, bnmodules): 
+    for i in xrange(0,len(convparams)):
+        param = net.params[convparams[i]]
+        weight = convmodules[i].weight.asNumpyArray()
+        print param[0].data.shape
+        print weight.shape
+        param[0].data.flat = weight.flat
+        if len(param) == 2:
+            bias = convmodules[i].bias.asNumpyArray()
+            param[1].data.flat = bias.flat
+
+    for i in xrange(0, len(scaleparams)):
+        scparam = net.params[scaleparams[i]]
+        weight = bnmodules[i].weight.asNumpyArray()
+        scparam[0].data.flat = weight.flat
+        bias = bnmodules[i].bias.asNumpyArray()
+        scparam[1].data.flat = bias.flat
+        """
+        bnparam = net.params[bnparams[i]]
+        mean = bnmodules[i].running_mean.asNumpyArray()
+        bnparam[0].data.flat = mean.flat
+        var = bnmodules[i].running_var.asNumpyArray()
+        bnparam[1].data.flat = var.flat
+        """
 require('nn')
 require('cudnn')
 require('cutorch')
@@ -25,5 +49,6 @@ for i in xrange(0,1000):
         convmodules.append(modules[i])
 
 cl, bl, sl = m2list.InceptionV3Body()
-modules2net.modules2net(net, cl, [], convmodules, [])
+modules2params(net, cl, [], convmodules, [])
+
 
